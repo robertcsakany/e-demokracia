e730ee594acbe9a9ca70191f2f8e5601823669da
Robert Csakany <csakany.robert@gmail.com>
2023-04-18 21:38:21 +0200

    Fixing upload script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-scp ./application/docker/target/edemokracia-application-*_docker_image.tar edemokracia.judo.technology:/tmp/edemokracia_image.tar
+scp ./application/docker/target/edemokracia-application-*_docker_image_x86_64.tar edemokracia.judo.technology:/tmp/edemokracia_image.tar
+
 scp ./update-image.sh edemokracia.judo.technology:/tmp/update-image.sh
 
 ssh edemokracia.judo.technology /tmp/update-image.sh
